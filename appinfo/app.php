b361fdd7dfc195b9ca027197c47099ecfd6fd767
NastuzziSamy <samy@nastuzzi.fr>
2018-02-05 01:38:54 +0100

    Optimize script export

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,3 +1,8 @@
 <?php
-  \OCP\Util::addScript('files_rightclick', 'script');
+  \OC::$server->getEventDispatcher()->addListener(
+    'OCA\Files::loadAdditionalScripts',
+    function () {
+      \OCP\Util::addScript('files_rightclick', 'script');
+    }
+  );
 ?>
