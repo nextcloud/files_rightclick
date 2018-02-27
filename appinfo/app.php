334811a72da97dc7a1b14037d4a0f8ba0adb972f
Nastuzzi Samy <nastuzzi.s@gmail.com>
2018-02-27 15:26:13 +0100

    Add pdf open option and add files_rightclick to files_share

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,6 +1,16 @@
 <?php
-  \OC::$server->getEventDispatcher()->addListener(
+
+  $eventDispatcher = \OC::$server->getEventDispatcher();
+
+  $eventDispatcher->addListener(
     'OCA\Files::loadAdditionalScripts',
+    function () {
+      \OCP\Util::addScript('files_rightclick', 'script');
+    }
+  );
+
+  $eventDispatcher->addListener(
+    'OCA\Files_Sharing::loadAdditionalScripts',
     function () {
       \OCP\Util::addScript('files_rightclick', 'script');
     }
