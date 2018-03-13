a59156d6d85ce96d7f4ad319f0e8d3a45d5d7e01
Dário Estevão <darioems@gmail.com>
2018-03-13 09:13:24 -0300

    Version for Portuguese Brazil
    Add translation for portuguese Brazil

---
+++ l10n/pt_br.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+  "files_rightclick", {
+    "Open this folder" : "Abrir esta pasta",
+    "Open this file" : "Abrir este arquivo",
+    "Open in a new tab" : "Abrir em uma nova aba",
+    "Edit this file" : "Editar este arquivo",
+    "Read this PDF" : "Ler este PDF",
+    "See this picture" : "Ver esta imagem",
+    "Open in the gallery app" : "Abrir na Galeria",
+    "Start playing" : "Comece a tocar",
+    "Stop playing" : "Parar de tocar",
+    "Start watching" : "Comece a assistir",
+    "Share this folder" : "Compartilhar esta pasta",
+    "Share this file" : "Compartilhar este arquivo",
+  },
+  "nplurals=2; plural=(n > 1);"
+);
