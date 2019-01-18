3b1a1a8f23194bcbf927c327b999dd8f2888fbbd
Jo Klaps <jo.klaps@gmail.com>
2019-01-18 13:51:38 +0100

    Create nl.js

---
+++ l10n/nl.js
@@ -0,0 +1,19 @@
+OC.L10N.register(
+  "files_rightclick", {
+    "Open folder" : "Open map",
+    "Open file" : "Open bestand",
+    "Open in new tab" : "In nieuw tab openen",
+    "Edit file" : "Bestand bewerken",
+    "Read PDF" : "PDF openen",
+    "See picture" : "Afbeelding bekijken",
+    "Open in Gallery" : "Openen in galerij",
+    "Play" : "Afspelen",
+    "Stop playback" : "Afspelen stoppen",
+    "Watch" : "Video bekijken",
+    "Share folder" : "Map delen",
+    "Share file" : "Bestand delen",
+    "Select" : "Selecteren",
+    "Unselect" : "Selectie opheffen",
+  },
+  "nplurals=2; plural=(n > 1);"
+);
