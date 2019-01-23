0e8c4d7b25df57f514e91c33012e2cce8997626f
Drakhart <drakhart@hotmail.com>
2019-01-23 23:54:44 +0100

    Add Spanish translations

---
+++ l10n/es.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+  "files_rightclick", {
+    "Open folder": "Abrir carpeta",
+    "Open file": "Abrir archivo",
+    "Open in new tab": "Abrir en una pestaña nueva",
+    "Edit file": "Editar archivo",
+    "Read PDF": "Leer PDF",
+    "See picture": "Ver imagen",
+    "Open in Gallery": "Abrir en la Galería",
+    "Play": "Reproducir",
+    "Play/Pause": "Reproducir/Pausar",
+    "Stop playback": "Detener reproducción",
+    "Watch": "Ver vídeo",
+    "Share folder": "Compartir carpeta",
+    "Share file": "Compartir archivo",
+	  "Select": "Seleccionar",
+	  "Unselect": "Deseleccionar",
+    "Get WebDAV link": "Obtener enlace WebDAV",
+    "Copied !": "¡Copiado!"
+  },
+  "nplurals=2; plural=(n > 1);"
+);
