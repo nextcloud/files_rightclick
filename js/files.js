5d31490e198388d2e5b5c544c0f22aba302e8563
Vincent Petry <vincent@nextcloud.com>
2022-09-13 09:17:40 +0200

    Fix file list selectors
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>

--- js/files.js
+++ js/files.js
@@ -8,7 +8,7 @@
 
     var appName = RightClick.appName;
 
-    new RightClick.Menu($('tbody[id=fileList]'), function (event, context, delimiter) {
+    new RightClick.Menu($('tbody[class=files-fileList]'), function (event, context, delimiter) {
         var options = new RightClick.Options();
         var currentFile = $(event.target).closest('tr');
         var selectedActions = '.selectedActions .menu-center li';
@@ -116,7 +116,7 @@
         }, 100);
 
         return options;
-    }, $('#app-content-files #fileList'), function () {
+    }, $('#app-content-files .files-fileList'), function () {
         $('.filesSelectMenu').css('visibility', 'visible');
     });
 })(window, jQuery, RightClick);
